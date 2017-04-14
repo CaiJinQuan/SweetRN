e135367abb6f457fba2f815e8ce123d921f652b9
apple <jikuan.chang@gmail.com>
2017-04-14 17:40:37 +0800

    Initial Commit

---
+++ SweetRN_110/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SweetRN_110
+//
+//  Created by apple on 2017/4/14.
+//  Copyright © 2017年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ SweetRN_110Tests/SweetRN_110Tests.m
@@ -0,0 +1,39 @@
+//
+//  SweetRN_110Tests.m
+//  SweetRN_110Tests
+//
+//  Created by apple on 2017/4/14.
+//  Copyright © 2017年 apple. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SweetRN_110Tests : XCTestCase
+
+@end
+
+@implementation SweetRN_110Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
